59faa765abf7fe72cc730c15e9b00ed2f6199a64
laike9m <laike9m@gmail.com>
2020-04-13 01:27:14 -0700

    Support import

---
+++ test/test_import.py
@@ -0,0 +1,32 @@
+from hamcrest import *  # IMPORT_STAR
+
+
+def test_import(tracer):
+    tracer.init()
+
+    import os  # IMPORT_NAME
+    from os import path  # IMPORT_FROM, loads module
+    from sys import settrace  # IMPORT_FROM, loads function
+
+    tracer.register()
+
+    assert_that(
+        tracer.logger.mutations,
+        contains_exactly(
+            has_properties(
+                {
+                    "target": "os",
+                    "value": starts_with("<module 'os'"),
+                    "sources": set(),
+                }
+            ),
+            has_properties(
+                {
+                    "target": "path",
+                    "value": all_of(starts_with("<module"), contains_string("path")),
+                    "sources": set(),
+                }
+            ),
+            has_properties({"target": "settrace", "value": settrace, "sources": set()}),
+        ),
+    )
--- test/test_miscellaneous.py
+++ test/test_miscellaneous.py
@@ -3,17 +3,18 @@ def test_miscellaneous(tracer):
     b = "b"
     c = "c"
     d = "d"
+    e = [1, 2, 3]
 
     tracer.init()
 
     x = f"{a} {b:4} {c!r} {d!r:4}"  # FORMAT_VALUE, BUILD_STRING
-    x = a == b == c  # ROT_THREE, COMPARE_OP
+    x = a == b == c  # ROT_THREE, _COMPARE_OP
+    e[0] += e.pop()  # DUP_TOP_TWO
 
     tracer.register()
-
-    print(tracer.logger.mutations)
 
     assert tracer.logger.mutations == [
         {"target": "x", "value": "a b    'c' 'd' ", "sources": {"a", "b", "d", "c"}},
         {"target": "x", "value": False, "sources": {"a", "b"}},
+        {"target": "e", "value": [4, 2], "sources": {"e"}},
     ]
