f31b1a8728ba52d820347c835deabb10b8b99fcb
laike9m <laike9m@gmail.com>
2020-07-06 00:32:47 -0700

    Renamed extension start command

--- cyberbrain-vsc/src/extension.ts
+++ cyberbrain-vsc/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cyberbrain.helloWorld', async () => {
+	let disposable = vscode.commands.registerCommand('cyberbrain.init', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from cyberbrain!');
