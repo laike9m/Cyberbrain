6ba0fbef41b073c3731ec253e598cfe145e23920
laike9m <laike9m@gmail.com>
2020-07-12 15:46:31 -0700

    Some renaming, defined fields for FrameLocater

--- cyberbrain-vsc/src/generated/communication_grpc_pb.d.ts
+++ cyberbrain-vsc/src/generated/communication_grpc_pb.d.ts
@@ -8,8 +8,8 @@ import * as grpc from "@grpc/grpc-js";
 
 interface ICommunicationService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
   syncState: grpc.MethodDefinition<communication_pb.State, communication_pb.State>;
-  findFrame: grpc.MethodDefinition<communication_pb.CursorPosition, communication_pb.FrameLocaterList>;
-  getFrameBackTrace: grpc.MethodDefinition<communication_pb.FrameLocater, communication_pb.Frame>;
+  findFrames: grpc.MethodDefinition<communication_pb.CursorPosition, communication_pb.FrameLocaterList>;
+  getFrame: grpc.MethodDefinition<communication_pb.FrameLocater, communication_pb.Frame>;
 }
 
 export const CommunicationService: ICommunicationService;
@@ -18,10 +18,10 @@ export class CommunicationClient extends grpc.Client {
   constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
   syncState(argument: communication_pb.State, metadataOrOptions?: grpc.Metadata | grpc.CallOptions | null): grpc.ClientReadableStream<communication_pb.State>;
   syncState(argument: communication_pb.State, metadata?: grpc.Metadata | null, options?: grpc.CallOptions | null): grpc.ClientReadableStream<communication_pb.State>;
-  findFrame(argument: communication_pb.CursorPosition, callback: grpc.requestCallback<communication_pb.FrameLocaterList>): grpc.ClientUnaryCall;
-  findFrame(argument: communication_pb.CursorPosition, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.FrameLocaterList>): grpc.ClientUnaryCall;
-  findFrame(argument: communication_pb.CursorPosition, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.FrameLocaterList>): grpc.ClientUnaryCall;
-  getFrameBackTrace(argument: communication_pb.FrameLocater, callback: grpc.requestCallback<communication_pb.Frame>): grpc.ClientUnaryCall;
-  getFrameBackTrace(argument: communication_pb.FrameLocater, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.Frame>): grpc.ClientUnaryCall;
-  getFrameBackTrace(argument: communication_pb.FrameLocater, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.Frame>): grpc.ClientUnaryCall;
+  findFrames(argument: communication_pb.CursorPosition, callback: grpc.requestCallback<communication_pb.FrameLocaterList>): grpc.ClientUnaryCall;
+  findFrames(argument: communication_pb.CursorPosition, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.FrameLocaterList>): grpc.ClientUnaryCall;
+  findFrames(argument: communication_pb.CursorPosition, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.FrameLocaterList>): grpc.ClientUnaryCall;
+  getFrame(argument: communication_pb.FrameLocater, callback: grpc.requestCallback<communication_pb.Frame>): grpc.ClientUnaryCall;
+  getFrame(argument: communication_pb.FrameLocater, metadataOrOptions: grpc.Metadata | grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.Frame>): grpc.ClientUnaryCall;
+  getFrame(argument: communication_pb.FrameLocater, metadata: grpc.Metadata | null, options: grpc.CallOptions | null, callback: grpc.requestCallback<communication_pb.Frame>): grpc.ClientUnaryCall;
 }
