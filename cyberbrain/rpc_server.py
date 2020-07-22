895c848412b0437aef50396d306d22f8d54c3ee5
laike9m <laike9m@gmail.com>
2020-07-22 03:11:58 -0700

    Server can return events, more tests to be added.

--- cyberbrain/rpc_server.py
+++ cyberbrain/rpc_server.py
@@ -7,9 +7,58 @@ from threading import Timer
 
 import grpc
 
+from .basis import Event, InitialValue, Creation, Mutation, Deletion
 from .frame_tree import FrameTree
 from .generated import communication_pb2
 from .generated import communication_pb2_grpc
+
+
+def transform_event_to_proto(event: Event) -> communication_pb2.Event:
+    event_proto = communication_pb2.Event()
+    if isinstance(event, InitialValue):
+        event_proto.initial_value.CopyFrom(
+            communication_pb2.InitialValue(
+                uid=event.uid,
+                filename=event.filename,
+                lineno=event.lineno,
+                target=event.target,
+                value=str(event.value),  # TODO: to JSON.
+            )
+        )
+    elif isinstance(event, Creation):
+        event_proto.creation.CopyFrom(
+            communication_pb2.Creation(
+                uid=event.uid,
+                filename=event.filename,
+                lineno=event.lineno,
+                target=event.target,
+                value=str(event.value),
+                sources=sorted(event.sources),  # Sorted to make result deterministic.
+            )
+        )
+    elif isinstance(event, Mutation):
+        event_proto.mutation.CopyFrom(
+            communication_pb2.Mutation(
+                uid=event.uid,
+                filename=event.filename,
+                lineno=event.lineno,
+                target=event.target,
+                value=str(event.value),
+                delta=str(event.delta.to_dict()),  # TODO: To JSON
+                sources=sorted(event.sources),
+            )
+        )
+    elif isinstance(event, Deletion):
+        event_proto.deletion.CopyFrom(
+            communication_pb2.Deletion(
+                uid=event.uid,
+                filename=event.filename,
+                lineno=event.lineno,
+                target=event.target,
+            )
+        )
+
+    return event_proto
 
 
 class CyberbrainCommunicationServicer(communication_pb2_grpc.CommunicationServicer):
@@ -17,7 +66,7 @@ class CyberbrainCommunicationServicer(communication_pb2_grpc.CommunicationServic
     state_queue = queue.Queue()
 
     def SyncState(self, request, context):
-        print(f"request come: {type(request)} {request}")
+        print(f"Received request SyncState: {type(request)} {request}")
         yield communication_pb2.State(status=communication_pb2.State.SERVER_READY)
         Timer(
             5,  # seconds
@@ -35,12 +84,20 @@ class CyberbrainCommunicationServicer(communication_pb2_grpc.CommunicationServic
         frames = FrameTree.find_frames(request)
         raise NotImplementedError
 
-    def GetFrame(self, request, context) -> communication_pb2.Frame:
-        # TODO: return accumulated_events + tracing result.
-        #  1. Get a frame from frame tree.
-        #  2. Get accumulated_events + tracing result.
-        #  3. Transforms to Frame proto.
-        pass
+    def GetFrame(
+        self, request: communication_pb2.FrameLocater, context
+    ) -> communication_pb2.Frame:
+        # TODO: return tracing result.
+        print(f"Received request GetFrame: {type(request)} {request}")
+        frame = FrameTree.get_frame(request.frame_name)
+        frame_proto = communication_pb2.Frame(filename=frame.filename)
+        for identifier, events in frame.accumulated_events.items():
+            event_list = communication_pb2.EventList()
+            event_list.events.extend(
+                [transform_event_to_proto(event) for event in events]
+            )
+            frame_proto.events[identifier].CopyFrom(event_list)
+        return frame_proto
 
 
 class Server:
@@ -49,18 +106,26 @@ class Server:
         communication_pb2_grpc.add_CommunicationServicer_to_server(
             CyberbrainCommunicationServicer(), self._server
         )
+        self._port = None
 
-    def serve(self, block: bool, port: int = 50051):
-        print("Starting grpc server...")
+    def serve(self, port: int = 50051):
+        print(f"Starting grpc server on {port}...")
+        self._port = port
         self._server.add_insecure_port(f"[::]:{port}")
         self._server.start()
-        if block:
-            self._server.wait_for_termination()
+
+    def wait_for_termination(self):
+        self._server.wait_for_termination()
 
     def stop(self):
         self._server.stop(grace=None)  # Abort all active RPCs immediately.
 
+    @property
+    def port(self):
+        return self._port
+
 
 if __name__ == "__main__":
     server = Server()
-    server.serve(block=True)
+    server.serve()
+    server.wait_for_termination()
