2be5468d9498e0524a7c6053298c5b58f51d44a5
laike9m <laike9m@gmail.com>
2020-07-28 21:40:13 -0700

    Implemented a fake version of FindFrames

--- cyberbrain-vsc/src/messaging.ts
+++ cyberbrain-vsc/src/messaging.ts
@@ -3,9 +3,10 @@ Handles incoming messages from Python and forwards them to webview.
 */
 
 import * as grpc from "@grpc/grpc-js";
-import {State} from "./generated/communication_pb";
+import {CursorPosition, State} from "./generated/communication_pb";
 import {postMessageToBacktracePanel} from "./webview";
 import {RpcClient} from "./rpc_client";
+import {Position, TextDocument} from "vscode";
 
 
 export class MessageCenter {
@@ -22,9 +23,18 @@ export class MessageCenter {
         let state = new State();
         state.setStatus(State.Status.CLIENT_READY);
         this.handleServerState(this.rpcClient.syncState(state));
+
+        // TODO: Pass real cursor position.
+        await this.findFrames(undefined,undefined);
     }
 
-    handleServerState(call: grpc.ClientReadableStream<State>) {
+    async findFrames(document?: TextDocument, position?: Position) {
+        console.log(`Calling findFrames with ${document}, ${position}`);
+        let locaters = await this.rpcClient.findFrames(new CursorPosition());
+        console.log(`frame locaters are: ${locaters}`);
+    }
+
+    private handleServerState(call: grpc.ClientReadableStream<State>) {
         call.on('data', function (serverState: State) {
             switch (serverState?.getStatus()) {
                 case State.Status.SERVER_READY:
