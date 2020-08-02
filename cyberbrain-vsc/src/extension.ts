ebb94f3a9fbb1f356d2df8b72df0f52ed95dd876
laike9m <laike9m@gmail.com>
2020-08-01 18:00:39 -0700

    Implemented getFrame in rpc client

--- cyberbrain-vsc/src/extension.ts
+++ cyberbrain-vsc/src/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {activateWebView} from "./webview";
-import {MessageCenter} from "./messaging";
+import { activateWebView } from "./webview";
+import { MessageCenter } from "./messaging";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,8 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('cyberbrain.init', async () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
+        console.clear();
         vscode.window.showInformationMessage('Hello World from cyberbrain!');
         let messageCenter = new MessageCenter();
         await messageCenter.start();
@@ -23,8 +22,8 @@ export function activate(context: vscode.ExtensionContext) {
     activateWebView(context);
 
     // TODO: do nothing if devtools is already opened. Move to webview.ts
-    vscode.commands.executeCommand('workbench.action.toggleDevTools');
-    console.log([1, 2, 3, 4]);
+    // Should be easy once https://github.com/microsoft/vscode/issues/103610 is fixed.
+    // vscode.commands.executeCommand('workbench.action.toggleDevTools');
 }
 
 // this method is called when your extension is deactivated
