361e8e5e3dd6dbff5e68e02d9e4e85280b00ad02
laike9m <laike9m@gmail.com>
2020-08-04 00:30:38 -0700

    Show dummy graph

--- cyberbrain-vsc/src/extension.ts
+++ cyberbrain-vsc/src/extension.ts
@@ -10,7 +10,7 @@ export function activate(context: vscode.ExtensionContext) {
     async () => {
       let webviewPanel = activateWebView(context);
       let messageCenter = new MessageCenter(context, webviewPanel);
-      await messageCenter.start();
+      await messageCenter.wait();
     }
   );
 
