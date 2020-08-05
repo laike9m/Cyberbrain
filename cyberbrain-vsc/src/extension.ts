6fc8d039e150c033f653cbc4ed1d3c22109b9d1a
laike9m <laike9m@gmail.com>
2020-08-04 21:52:11 -0700

    Optimized webview initialization

--- cyberbrain-vsc/src/extension.ts
+++ cyberbrain-vsc/src/extension.ts
@@ -1,15 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { activateWebView } from "./webview";
 import { MessageCenter } from "./messaging";
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand(
     "cyberbrain.init",
     async () => {
-      let webviewPanel = activateWebView(context);
-      let messageCenter = new MessageCenter(context, webviewPanel);
+      let messageCenter = new MessageCenter(context);
       await messageCenter.wait();
     }
   );
