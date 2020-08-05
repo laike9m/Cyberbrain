014d51bff5b58527d6e494a8d8d95887d5b1601f
laike9m <laike9m@gmail.com>
2020-08-04 23:18:17 -0700

    Create a separate visualize.js file

---
+++ cyberbrain-vsc/src/visualize.js
@@ -0,0 +1,43 @@
+/*
+If you want to modify this file, you may find it hard because the vis-network lib
+is not imported directly, so you won't have auto-complete. Due to the limitation
+ of vscode,
+(https://github.com/microsoft/vscode/issues/72900#issuecomment-487140263)
+it's impossible to import a file, as it requires sending http requests.
+
+So I pretty much just rely on my IDE (PyCharm in this case) to provide the
+definitions for me. In "Languages & Frameworks > JavaScript > Libraries", you
+can add the local vis-network.js file as a library, so that PyCharm recognizes
+the vis object, and can provide auto-complete. See
+https://i.loli.net/2020/08/05/FBqXQpjk4YVLGRa.png
+
+For other IDEs/editors, I believe it's possible to achieve a similar effect.
+*/
+
+window.addEventListener("message", (event) => {
+  console.log(event.data);
+
+  let nodes = new vis.DataSet([
+    { id: 1, label: "Node 1" },
+    { id: 2, label: "Node 2" },
+    { id: 3, label: "Node 3" },
+    { id: 4, label: "Node 4" },
+    { id: 5, label: "Node 5" },
+  ]);
+
+  const edges = new vis.DataSet([
+    { from: 1, to: 3 },
+    { from: 1, to: 2 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 },
+    { from: 3, to: 3 },
+  ]);
+
+  const container = document.getElementById("vis");
+  const data = {
+    nodes: nodes,
+    edges: edges,
+  };
+  const options = {};
+  const network = new vis.Network(container, data, options);
+});
