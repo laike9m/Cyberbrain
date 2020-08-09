4d464feb172119b14b552169f20052c3bbfdd24d
laike9m <laike9m@gmail.com>
2020-08-08 17:07:19 -0700

    Build lineno nodes

--- cyberbrain-vsc/src/visualize.js
+++ cyberbrain-vsc/src/visualize.js
@@ -19,6 +19,10 @@ I also tried using making this file a ts file but met a few difficulties:
    I'd like to avoid at least for now.
 */
 
+let backGroundColor = window
+  .getComputedStyle(document.body, null)
+  .getPropertyValue("background-color");
+
 const options = {
   edges: {
     smooth: {
@@ -26,22 +30,51 @@ const options = {
       forceDirection: "vertical",
     },
   },
+  interaction: {
+    dragNodes: false,
+  },
   layout: {
     hierarchical: {
       direction: "UD", // From up to bottom.
     },
   },
-  physics: false,
+  physics: {
+    hierarchicalRepulsion: {
+      avoidOverlap: 1, // puts the most space around nodes to avoid overlapping.
+    },
+  },
 };
 
 window.addEventListener("message", (event) => {
   console.log(event.data);
+
+  let lines = new Set();
 
   let events = event.data.events;
   let nodes = new vis.DataSet([]);
   for (let identifier in events) {
     if (Object.prototype.hasOwnProperty.call(events, identifier)) {
       for (let event of events[identifier]) {
+        let linenoString = event.lineno.toString();
+        if (!lines.has(linenoString)) {
+          // Adds a "virtual node" to show line number. This node should precede other nodes
+          // on the same level. According to https://github.com/visjs/vis-network/issues/926,
+          // the order is not deterministic, but seems it's roughly the same as the insertion
+          // order.
+          lines.add(linenoString);
+          nodes.add({
+            id: linenoString,
+            level: event.lineno,
+            label: linenoString,
+            borderWidth: 0,
+            // Disable physics so the lineno nodes are not pushed away to the left.
+            physics: false,
+            color: {
+              border: backGroundColor,
+              background: backGroundColor,
+            },
+          });
+        }
         nodes.add({
           id: event.uid,
           level: event.lineno,
@@ -51,12 +84,29 @@ window.addEventListener("message", (event) => {
     }
   }
 
-  let tracingResult = event.data.tracingResult;
   const edges = new vis.DataSet([]);
+
+  // Add hidden edges so that lineno nodes are placed on the same vertical position.
+  lines = Array.from(lines);
+  lines.sort();
+  for (let i = 0; i < lines.length - 1; i++) {
+    edges.add({
+      from: lines[i],
+      to: lines[i + 1],
+      color: {
+        color: backGroundColor,
+      },
+    });
+  }
+
+  let tracingResult = event.data.tracingResult;
   for (let event_uid in tracingResult) {
     if (Object.prototype.hasOwnProperty.call(tracingResult, event_uid)) {
       for (let source_event_uid of tracingResult[event_uid]) {
-        edges.add({ from: source_event_uid, to: event_uid });
+        edges.add({
+          from: source_event_uid,
+          to: event_uid,
+        });
       }
     }
   }
@@ -68,6 +118,8 @@ window.addEventListener("message", (event) => {
   };
   const network = new vis.Network(container, data, options);
 });
+
+///////////////////////// Helper functions /////////////////////////
 
 function buildLabelText(event) {
   return `${event.target}: ${event.type} : ${event.uid}`;
