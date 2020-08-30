32b5002036a1594a3f16a95c7b03f6e185bab3ae
laike9m <laike9m@gmail.com>
2020-08-30 00:24:35 -0700

    Now frontend uses the new events format

--- cyberbrain-vsc/src/frame.ts
+++ cyberbrain-vsc/src/frame.ts
@@ -1,15 +1,13 @@
 /* Holding a frame passed from backend and handles console logging. */
 
-import {
-  Event as EventProto,
-  Frame as FrameProto,
-} from "./generated/communication_pb";
+import { Frame as FrameProto } from "./generated/communication_pb";
 
 import { Binding, Deletion, Event, InitialValue, Mutation } from "./basis";
 
 export class Frame {
   filename?: string;
-  events: Record</* identifier */ string, Event[]> = {};
+  events: Array<Event> = [];
+  identifiers: Array<string> = [];
 
   // Maps events to relevant predecessor events.
   tracingResult: Record<
@@ -19,28 +17,21 @@ export class Frame {
 
   constructor(frame: FrameProto) {
     this.filename = frame.getFilename();
-    frame.getEventsMap().forEach((eventList, identifier) => {
-      let event: EventProto;
-      if (!this.events.hasOwnProperty(identifier)) {
-        this.events[identifier] = [];
+    frame.getEventsList().forEach((event) => {
+      if (event.hasInitialValue()) {
+        this.events.push(new InitialValue(event.getInitialValue()!));
       }
-      for (event of eventList.getEventsList()) {
-        if (event.hasInitialValue()) {
-          this.events[identifier].push(
-            new InitialValue(event.getInitialValue()!)
-          );
-        }
-        if (event.hasBinding()) {
-          this.events[identifier].push(new Binding(event.getBinding()!));
-        }
-        if (event.hasMutation()) {
-          this.events[identifier].push(new Mutation(event.getMutation()!));
-        }
-        if (event.hasDeletion()) {
-          this.events[identifier].push(new Deletion(event.getDeletion()!));
-        }
+      if (event.hasBinding()) {
+        this.events.push(new Binding(event.getBinding()!));
+      }
+      if (event.hasMutation()) {
+        this.events.push(new Mutation(event.getMutation()!));
+      }
+      if (event.hasDeletion()) {
+        this.events.push(new Deletion(event.getDeletion()!));
       }
     });
+    this.identifiers = frame.getIdentifiersList();
     frame.getTracingResultMap().forEach((predecessorEventUidList, eventUid) => {
       this.tracingResult[eventUid] = predecessorEventUidList.getEventUidsList();
     });
