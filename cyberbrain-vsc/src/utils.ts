5c30bb4dde923d389feaaa32bb377f4df74f9735
laike9m <laike9m@gmail.com>
2020-09-04 18:54:41 -0700

    Init cb automatically during development

--- cyberbrain-vsc/src/utils.ts
+++ cyberbrain-vsc/src/utils.ts
@@ -1,8 +1,16 @@
+import * as vscode from "vscode";
 
 // TODO: Learn from
 //   https://github.com/cuthbertLab/jsonpickleJS/blob/master/js/unpickler.js
 //   and implement a better version of decode.
 //   Maybe use https://stackoverflow.com/a/46132163.
 export function decodeJson(jsonString: string) {
-    return JSON.parse(jsonString);
+  return JSON.parse(jsonString);
+}
+
+export function doIfInDevMode(context: vscode.ExtensionContext, callback: any) {
+  // ExtensionMode: Production = 1, Development = 2, Test = 3.
+  if (context.extensionMode === 2) {
+    callback();
+  }
 }
