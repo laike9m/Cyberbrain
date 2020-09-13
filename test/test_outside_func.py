cbb4056bfba2ee3119e37f66070c49b072b38fed
laike9m <laike9m@gmail.com>
2020-09-13 02:44:37 -0700

    Hide the Tracer class, because we don't want users to instantiate it directly #15

--- test/test_outside_func.py
+++ test/test_outside_func.py
@@ -1,9 +1,8 @@
 """Certain instructions can only be tested outside of a function."""
 
-from cyberbrain import Tracer, InitialValue, Deletion, Mutation, Symbol
+from cyberbrain import tracer, InitialValue, Deletion, Mutation, Symbol
 from utils import assert_GetFrame
 
-tracer = Tracer()
 
 x = 1
 
@@ -15,14 +14,14 @@ tracer.stop()
 
 def test_module(rpc_stub):
     assert tracer.events == [
-        InitialValue(target=Symbol("x"), value=1, lineno=11),
-        Deletion(target=Symbol("x"), lineno=11),
-        InitialValue(target=Symbol("__annotations__"), value={}, lineno=12),
+        InitialValue(target=Symbol("x"), value=1, lineno=10),
+        Deletion(target=Symbol("x"), lineno=10),
+        InitialValue(target=Symbol("__annotations__"), value={}, lineno=11),
         Mutation(
             target=Symbol("__annotations__"),
             value={"y": int},
             sources=set(),  # `int` is a built-in so is excluded from sources.
-            lineno=12,
+            lineno=11,
         ),
     ]
 
