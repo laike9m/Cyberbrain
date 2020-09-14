cfb07c981dddd739304077555d0ba94f3cde8cb9
laike9m <laike9m@gmail.com>
2020-09-13 18:15:47 -0700

    Users can now disable tracing, fixes #15

--- test/conftest.py
+++ test/conftest.py
@@ -9,12 +9,14 @@ def pytest_addoption(parser):
     parser.addoption("--debug_mode", action="store_true", default=False)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def tracer(request):
-    return _Tracer(request.config.getoption("--debug_mode"))
+    # We still create a new Tracer object for each test to make sure Tracer has a fresh
+    # state for each test.
+    return _Tracer(debug_mode=request.config.getoption("--debug_mode"))
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def rpc_stub(request):
     port_picked = request.getfixturevalue("tracer").server.port
     channel = grpc.insecure_channel(f"localhost:{port_picked}")
