76156982bc8dde1c4922b8703fbb0f72befd8471
laike9m <laike9m@gmail.com>
2020-09-13 18:15:47 -0700

    Users can now disable tracing, fixes #15

---
+++ test/test_api_disabled.py
@@ -0,0 +1,28 @@
+from cyberbrain import trace
+
+
+def test_disabled_tracer(tracer):
+    tracer.start(disabled=True)
+    a = 1
+    tracer.stop()
+
+    assert tracer.frame_logger is None, tracer.frame_logger
+
+
+def test_decorator_with_argument(rpc_stub):
+    @trace(disabled=True)
+    def decorated_func_disabled():
+        a = 1
+        return a
+
+    @trace(disabled=False)
+    def decorated_func_enabled():
+        a = 2
+        return a
+
+    decorated_func_disabled()
+    decorated_func_enabled()
+
+    from utils import assert_GetFrame
+
+    assert_GetFrame(rpc_stub, "decorated_func_enabled")
