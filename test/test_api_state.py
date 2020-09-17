91f1fe84cba97c1cf51e869cf9493f1b49d30489
laike9m <laike9m@gmail.com>
2020-09-17 00:35:25 -0700

    Added a test case for #6

--- test/test_api_state.py
+++ test/test_api_state.py
@@ -13,3 +13,17 @@ def test_call_tracer_multiple_times(tracer):
     assert tracer.events == [
         Binding(lineno=6, target=Symbol("a"), value=1, sources=set())
     ]
+
+
+def test_decorator_multiple_times(trace, rpc_stub):
+    @trace
+    def func():
+        a = 1
+
+    func()
+
+    func()
+
+    assert trace.events == [
+        Binding(lineno=21, target=Symbol("a"), value=1, sources=set())
+    ]
