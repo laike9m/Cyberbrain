76482a4604ffc546a6bc9a4217d8580f932400c3
laike9m <laike9m@gmail.com>
2020-09-19 22:48:30 -0700

    Can render trace graph in tests, #10

--- cyberbrain-vsc/src/test/suite/extension.test.ts
+++ cyberbrain-vsc/src/test/suite/extension.test.ts
@@ -1,12 +1,21 @@
 import * as vscode from "vscode";
+import * as path from "path";
 
-suite('Extension Test Suite', function() {
-	this.timeout(6000);
+let cl = console.log;
 
-	test('cat coding', (done) => {
-		vscode.commands.executeCommand("cyberbrain.init");
-		setTimeout(() => {
-			done();
-		}, 5000);
-	});
+const fileBeingTraced = path.resolve(__dirname, "../../../../examples/loop.py");
+
+vscode.workspace.openTextDocument(fileBeingTraced).then(doc => {
+  vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
+});
+
+suite("Extension Test Suite", function() {
+  this.timeout(6000);
+
+  test("Render Trace Graph", done => {
+    vscode.commands.executeCommand("cyberbrain.init");
+    setTimeout(() => {
+      done();
+    }, 5000);
+  });
 });
