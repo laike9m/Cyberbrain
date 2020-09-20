d6cfe3b6654658eab8e1974503336940539be484
laike9m <laike9m@gmail.com>
2020-09-20 00:28:32 -0700

    Now can launch Python server from vsc tests, #10

--- cyberbrain-vsc/src/test/suite/extension.test.ts
+++ cyberbrain-vsc/src/test/suite/extension.test.ts
@@ -1,9 +1,28 @@
 import * as vscode from "vscode";
 import * as path from "path";
+import { spawn } from "child_process";
 
 let cl = console.log;
 
-const fileBeingTraced = path.resolve(__dirname, "../../../../examples/loop.py");
+const cbRoot = path.resolve(__dirname, "../../../..");
+
+const fileBeingTraced = path.resolve(cbRoot, "examples/loop.py");
+
+let serverProcess = spawn("python", ["-m", "examples.loop"], {
+  cwd: cbRoot
+});
+
+serverProcess.stdout.on("data", data => {
+  console.log(`stdout: ${data}`);
+});
+
+serverProcess.stderr.on("data", data => {
+  console.error(`stderr: ${data}`);
+});
+
+serverProcess.on("close", code => {
+  console.log(`Server exited with code ${code}`);
+});
 
 vscode.workspace.openTextDocument(fileBeingTraced).then(doc => {
   vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
@@ -14,6 +33,7 @@ suite("Extension Test Suite", function() {
 
   test("Render Trace Graph", done => {
     setTimeout(() => {
+      serverProcess.kill();
       done();
     }, 5000);
   });
