c03848ad714815d822c08f394bb63379792410fb
laike9m <laike9m@gmail.com>
2020-09-20 21:48:32 -0700

    Finally got integration tests working 🎉

--- cyberbrain-vsc/src/test/suite/extension.test.ts
+++ cyberbrain-vsc/src/test/suite/extension.test.ts
@@ -6,36 +6,58 @@ let cl = console.log;
 
 const cbRoot = path.resolve(__dirname, "../../../..");
 
-// const fileBeingTraced = path.resolve(cbRoot, "examples/loop.py");
-const fileBeingTraced = path.resolve(cbRoot, "examples/word_count/wc.py");
-
-let serverProcess = spawn("python", ["-m", "examples.word_count.wc"], {
-  cwd: cbRoot
-});
-
-serverProcess.stdout.on("data", data => {
-  console.log(`stdout: ${data}`);
-});
-
-serverProcess.stderr.on("data", data => {
-  console.error(`stderr: ${data}`);
-});
-
-serverProcess.on("close", code => {
-  console.log(`Server exited with code ${code}`);
-});
-
-vscode.workspace.openTextDocument(fileBeingTraced).then(doc => {
-  vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
-});
+const examples = [
+  { file: "examples/hello.py", args: ["-m", "examples.hello"] },
+  { file: "examples/loop.py", args: ["-m", "examples.loop"] },
+  {
+    file: "examples/word_count/wc.py",
+    args: ["-m", "examples.word_count.wc", "examples/word_count/inputs/*.txt"]
+  }
+];
 
 suite("Extension Test Suite", function() {
-  this.timeout(6000);
+  this.timeout(40000);
 
-  test("Render Trace Graph", done => {
-    setTimeout(() => {
-      serverProcess.kill();
-      done();
-    }, 5000);
+  test("Render Trace Graph", async function() {
+    for (const example of examples) {
+      await runTest(example);
+    }
   });
+
+  async function runTest(example: any) {
+    // Launches cb Python server.
+    const fileBeingTraced = path.resolve(cbRoot, example.file);
+    let serverProcess = spawn("python", example.args, {
+      cwd: cbRoot,
+      shell: true
+    });
+
+    serverProcess.stdout.on("data", data => {
+      console.log(`stdout: ${data}`);
+    });
+
+    serverProcess.stderr.on("data", data => {
+      console.error(`stderr: ${data}`);
+    });
+
+    serverProcess.on("close", code => {
+      console.log(`Server exited with code ${code}`);
+    });
+
+    // Opens the source file.
+    vscode.workspace.openTextDocument(fileBeingTraced).then(doc => {
+      vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
+    });
+
+    // Opens webview to show the trace graph.
+    vscode.commands.executeCommand("cyberbrain.init");
+
+    // Waits for 5 seconds so we have time to inspect the trace graph.
+    return new Promise(resolve => {
+      setTimeout(() => {
+        serverProcess.kill();
+        resolve();
+      }, 5000);
+    });
+  }
 });
