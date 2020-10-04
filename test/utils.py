8545ac5bd2a573880fdfb50f87a6af180f00076e
laike9m <laike9m@gmail.com>
2020-10-04 11:01:31 -0700

    Added a test to check serialized json

--- test/utils.py
+++ test/utils.py
@@ -18,6 +18,12 @@ def get_value(value_dict: dict[str, any]):
     return value_dict[python_version]
 
 
+def return_GetFrame(
+    rpc_stub: communication_pb2_grpc.CommunicationStub, frame_name: str
+) -> Frame:
+    return rpc_stub.GetFrame(communication_pb2.FrameLocater(frame_name=frame_name))
+
+
 def assert_GetFrame(
     rpc_stub: communication_pb2_grpc.CommunicationStub, frame_name: str
 ):
@@ -45,9 +51,7 @@ def assert_GetFrame(
     with open(golden_filepath, "rt") as f:
         response_text = f.read()
 
-    frame_proto = rpc_stub.GetFrame(
-        communication_pb2.FrameLocater(frame_name=frame_name)
-    )
+    frame_proto = return_GetFrame(rpc_stub, frame_name)
 
     assert frame_proto == text_format.Parse(
         response_text, communication_pb2.Frame()
