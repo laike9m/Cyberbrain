ce8fb27b68f3cde0b3096293e3ba0690c657ac4f
laike9m <laike9m@gmail.com>
2020-10-04 17:03:35 -0700

    Fixed a small problem, #8

--- examples/nonlocal.py
+++ examples/nonlocal.py
@@ -4,10 +4,14 @@ from cyberbrain import trace
 def main():
     x = 1
 
+    class A:
+        pass
+
     @trace
     def use_nonlocal():
         nonlocal x
         x = 10
+        a = A()
 
     use_nonlocal()
 
