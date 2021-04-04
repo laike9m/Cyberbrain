8c3f0a455efa0edff015cf275e7afdc5546ebb82
laike9m <laike9m@gmail.com>
2021-04-04 12:39:28 -0700

    Upgrade deps, fixed pandas tests

--- test/test_pandas.py
+++ test/test_pandas.py
@@ -15,7 +15,7 @@ def test_pandas(tracer, mocked_responses):
     df = pd.DataFrame(data=baby_data_set, columns=["Names", "Births"])
     tracer.stop()
 
-    from utils import get_value, get_os_type
+    from utils import get_value
 
     eol = get_value({"windows": r"\r\n", "linux": r"\n", "mac": "\\n"})
 
@@ -24,15 +24,28 @@ def test_pandas(tracer, mocked_responses):
             lineno=get_value({"py37": 13, "default": 8}),
             target=Symbol("baby_data_set"),
             value='[["Bob",968],["Jessica",155],["Mary",77],["John",578],["Mel",973]]',
-            repr="[('Bob',968),('Jessica',155),('Mary',77),('John',578),('Mel',973)]",
+            repr=(
+                "[('Bob', 968), ('Jessica', 155), "
+                "('Mary', 77), ('John', 578), ('Mel', 973)]"
+            ),
             sources=set(),
         ),
         Binding(
             lineno=15,
             target=Symbol("df"),
-            value=f'{{"values":"Names,Births{eol}Bob,968{eol}Jessica,155{eol}Mary,77{eol}John,578{eol}Mel,973{eol}"'
-            + ',"txt":true,"meta":{"dtypes":{"Names":"object","Births":"int64"},"index":"{\\"py/object\\":\\"pandas.core.indexes.range.RangeIndex\\",\\"values\\":\\"[0,1,2,3,4]\\",\\"txt\\":true,\\"meta\\":{\\"dtype\\":\\"int64\\",\\"name\\":null}}"}}',
-            repr="     Names  Births\n0      Bob     968\n1  Jessica     155\n2     Mary      77\n3     John     578\n4      Mel     973",
+            value=(
+                f'{{"values":"Names,Births{eol}Bob,968{eol}Jessica,155{eol}'
+                f'Mary,77{eol}John,578{eol}Mel,973{eol}"'
+                ',"txt":true,"meta":{"dtypes":{"Names":"object","Births":"int64"},'
+                '"index":"{\\"py/object\\":\\"pandas.core.indexes.range.RangeIndex\\"'
+                ',\\"values\\":\\"[0,1,2,3,4]\\",\\"txt\\":true,\\"meta\\":{\\"dtype\\"'
+                ':\\"int64\\",\\"name\\":null}}",'
+                '"column_level_names":[null],"header":[0]}}'
+            ),
+            repr=(
+                "     Names  Births\n0      Bob     968\n1  Jessica     155\n2"
+                "     Mary      77\n3     John     578\n4      Mel     973"
+            ),
             sources={Symbol("baby_data_set")},
         ),
     ]
