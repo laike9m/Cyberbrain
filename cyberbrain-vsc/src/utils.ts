02bd8845c1a67d9a9d13112535ea41a919da2a78
laike9m <laike9m@gmail.com>
2021-04-20 21:19:27 -0700

    Remove log file creation, fixed #96

--- cyberbrain-vsc/src/utils.ts
+++ cyberbrain-vsc/src/utils.ts
@@ -1,24 +1,4 @@
 import * as vscode from "vscode";
-import { configure, getLogger } from "log4js";
-
-const logFile = "/tmp/cyberbrain.log";
-
-configure({
-  appenders: {
-    fileAppender: {
-      type: "dateFile",
-      filename: logFile,
-      flags: "w" // Make sure each time an empty file is created.
-    }
-  },
-  categories: {
-    default: {
-      appenders: ["fileAppender"],
-      level: "all"
-    }
-  }
-});
-const logger = getLogger();
 
 export function isDevMode(context: vscode.ExtensionContext) {
   // ExtensionMode: Production = 1, Development = 2, Test = 3.
