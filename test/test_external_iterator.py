b9632da7a14fbb077c85b2ccddecd562656518bd
Victor Sun <victorjsun@gmail.com>
2021-07-11 17:59:49 -0400

    Add more tests

--- test/test_external_iterator.py
+++ test/test_external_iterator.py
@@ -12,41 +12,6 @@ def test_external_generator(trace, mocked_responses):
     
     external_generator_function()
 
-    # assert trace.events == [
-    #     InitialValue(
-    #         lineno=-1,
-    #         target=Symbol("inner"),
-    #         value='{"repr": "<function test_external_generator.<locals>.inner>"}',
-    #         repr="<function test_external_generator.<locals>.inner>",
-    #     ),
-    #     Binding(
-    #         lineno=106,
-    #         target=Symbol("generator"),
-    #         value='{"repr": "<generator object test_external_generator.<locals>.inner>"}',
-    #         repr="<generator object test_external_generator.<locals>.inner>",
-    #         sources=set(),
-    #     ),
-    #     Binding(
-    #         lineno=107,
-    #         target=Symbol("x"),
-    #         value="0",
-    #         repr="0",
-    #         sources={Symbol("generator")},
-    #     ),
-    #     Binding(
-    #         lineno=108,
-    #         target=Symbol("x"),
-    #         value="1",
-    #         repr="1",
-    #         sources={Symbol("generator")},
-    #     ),
-    #     Return(
-    #         lineno=109,
-    #         value="1",
-    #         repr="1",
-    #         sources={Symbol("x")},
-    #     ),
-    # ]
 
 def test_external_iterator(trace, mocked_responses):
     @trace
