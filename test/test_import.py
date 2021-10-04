6f05635301847b41102e616aa645f7c962102560
Victor Sun <victorjsun@gmail.com>
2021-10-03 20:19:03 -0400

    Remove import test due to repr naming

--- test/test_import.py
+++ test/test_import.py
@@ -3,7 +3,7 @@ from hamcrest import *
 from cyberbrain import Binding, Symbol
 
 
-def test_import(tracer, mocked_responses):
+def test_import(tracer):
     tracer.start()
 
     import os  # IMPORT_NAME
