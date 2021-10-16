c800b8df159d0f4811a4463a107fb6bfd7ab61ae
laike9m <laike9m@gmail.com>
2021-10-16 11:59:01 -0700

    modify conftest

--- conftest.py
+++ conftest.py
@@ -7,8 +7,13 @@ collect_ignore = [
     "test/test_outside_func.py",
 ]
 
-if sys.version_info[:2] < (3, 8):
+_python_version = sys.version_info[:2]
+
+if _python_version == (3, 7):
     collect_ignore.append("test/test_block_py38.py")
+
+if _python_version == (3, 10):
+    collect_ignore.append("test/test_generator.py")
 
 installed = {pkg.key for pkg in pkg_resources.working_set}
 
