d36352bd5cb806d7b53fa2d5d9419755daf4f8cd
Victor Sun <victorjsun@gmail.com>
2021-11-27 02:05:30 -0500

    Use try-import instead of pkg_resources (#145)

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
-import pkg_resources
 import sys
+import importlib
 
 # test_outside_func.py is ignored because it has code in global scope, and is always
 # executed if not ignored.
@@ -15,8 +15,8 @@ if _python_version == (3, 7):
 if _python_version == (3, 10):
     collect_ignore.append("test/test_generator.py")
 
-installed = {pkg.key for pkg in pkg_resources.working_set}
-
 for pkg in ("numpy", "pandas"):
-    if pkg not in installed:
+    try:
+        importlib.import_module(pkg)
+    except ImportError:
         collect_ignore.append(f"test/test_{pkg}.py")
